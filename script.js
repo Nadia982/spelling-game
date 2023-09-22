4d5394cb6ef52d5fe6630b05b939a10faf24c316
Natalie Gillam <ngillam@hotmail.co.uk>
2023-09-22 23:59:19 +0100

    CSS updates

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ const initialFunction = () => {
   const showChances = () => {
     let heartsLeft = new Array(lossCount);
     heartsLeft.fill('<ion-icon class="heart" name="heart"></ion-icon>');
-    userInpSectionChances.innerHTML = `<div id="chanceCount"><span>Chances left (for this word): ${lossCount}</span><span> ${heartsLeft.join(
+    userInpSectionChances.innerHTML = `<div id="chanceCount"><span>Chances left (for this word): ${lossCount}</span><span class="heartSpan"> ${heartsLeft.join(
       " "
     )}</span></div>`;
   };
